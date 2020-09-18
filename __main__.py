2d9cbd7eca0deaeac676814437c1d701ccea5872
Camilo Castro <camilo@ninjas.cl>
2020-09-18 00:18:18 -0300

    fixed lint

--- __main__.py
+++ __main__.py
@@ -2,20 +2,22 @@ import os
 import sys
 import chaucha
 
+
 def main():
 
     arg = ""
-    
+
     try:
         arg = sys.argv[1]
-    except:
+    except BaseException:
         pass
-    
+
     value = os.getenv("INPUT_VALUE") or arg
     privkey, pubkey = chaucha.wallet.new(value)
 
     print(f"::set-output name=privkey::{privkey}")
     print(f"::set-output name=pubkey::{pubkey}")
+
 
 if __name__ == "__main__":
     main()
